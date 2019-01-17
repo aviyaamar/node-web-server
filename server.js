ba358c377996d6d4f69777a69b8c2b9ff7360c38
aviyaamar <aviyaamar1@gmail.com>
2019-01-17 20:40:42 +0200

    add project page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -48,12 +50,18 @@ app.get('/about' ,(req, res) => {
   });
 });
 
+app.get('/projects', (req, res) =>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects'
+  });
+});
+
 app.get('/bad' , (req, res) => {
   res.send({
     errorMessage: 'unable to handle requset'
   });
 });
 
-app.listen(3000 , ()=>{
-  console.log('server is up on port 3000');
+app.listen(port, ()=>{
+  console.log(`server is up on port ${port}`);
 });
